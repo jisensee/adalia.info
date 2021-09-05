6cbaedc8e07cf6b8816166419d6c510667c2412d
Jannis Isensee <jannis.isensee@gmail.com>
2021-09-05 22:32:34 +0200

    add error handling to etherscan request

--- server/src/exchange-rates.ts
+++ server/src/exchange-rates.ts
@@ -14,13 +14,17 @@ let currentExchangeRates: ExchangeRates = {
 const etherscanPriceUrl =
   'https://api.etherscan.io/api?module=stats&action=ethprice'
 const updateExchangeRates = async () => {
-  const response = await fetch(etherscanPriceUrl)
-  const body = await response.json()
-  const rates = body as EtherscanResponse
-  currentExchangeRates = {
-    oneEthInUsd: rates.result.ethusd,
+  try {
+    const response = await fetch(etherscanPriceUrl)
+    const body = await response.json()
+    const rates = body as EtherscanResponse
+    currentExchangeRates = {
+      oneEthInUsd: rates.result.ethusd,
+    }
+    console.log('Updated exchange rates', currentExchangeRates)
+  } catch (err) {
+    console.log('Failed to update exchange rates', err)
   }
-  console.log('Updated exchange rates', currentExchangeRates)
 }
 
 updateExchangeRates()
