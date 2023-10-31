aea08267a4551c2c63e48b6a1e2b0fa256f338a4
Jannis Isensee <jannis.isensee@gmail.com>
2023-10-31 22:09:22 +0100

    setup prisma accelerate

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,4 +1,8 @@
 import { PrismaClient } from '@prisma/client'
-// import { withAccelerate } from '@prisma/extension-accelerate'
+import { withAccelerate } from '@prisma/extension-accelerate'
 
-export const db = new PrismaClient() //.$extends(withAccelerate())
+const client = new PrismaClient()
+
+export const db = process.env.VERCEL_URL
+  ? client.$extends(withAccelerate())
+  : client
