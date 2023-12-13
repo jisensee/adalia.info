56ed305183455bde5928d1edd62c77e9379b5f93
Jannis Isensee <jannis.isensee@gmail.com>
2023-12-13 23:39:41 +0100

    revalidate home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { LastAsteroidTransfers } from './last-asteroid-transfers'
 import { LastScannedAsteroids } from './last-scanned-asteroids'
 import { OwnedAsteroids } from './owned-asteroids'
 
+export const revalidate = 60
+
 export default async function Home() {
   return (
     <div className='flex flex-col gap-y-5 px-3'>
