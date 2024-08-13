7c139d3f96c6a92c659398a819f672e75e277633
Jannis Isensee <jannis.isensee@gmail.com>
2024-08-13 20:36:59 +0200

    clean up tools page

--- src/app/tools/page.tsx
+++ src/app/tools/page.tsx
@@ -1,71 +1,77 @@
+import {
+  Building,
+  ChartNoAxesCombined,
+  Cog,
+  Cpu,
+  LucideProps,
+  Rocket,
+  UsersRound,
+} from 'lucide-react'
+import { Route } from 'next'
 import Link from 'next/link'
+import { ComponentType } from 'react'
 
 export default function ToolsPage() {
   return (
     <div className='space-y-3 p-3'>
       <h1>Tools</h1>
-      <div className='grid grid-cols-1 gap-3 md:grid-cols-2'>
-        <Link href='/tools/process-finder'>
-          <div className='group flex flex-col items-center justify-center rounded border border-primary p-5 hover:bg-primary'>
-            <h2 className='group-hover:text-primary-foreground'>
-              Process Finder
-            </h2>
-            <p className='text-md group-hover:text-primary-foreground'>
-              Find out what you can make from your leftover products!
-            </p>
-          </div>
-        </Link>
-        <Link href='/tools/production-tracker'>
-          <div className='group flex flex-col items-center justify-center rounded border border-primary p-5 hover:bg-primary'>
-            <h2 className='group-hover:text-primary-foreground'>
-              Production Tracker
-            </h2>
-            <p className='text-md group-hover:text-primary-foreground'>
-              Easily keep track of all your running production facilities!
-            </p>
-          </div>
-        </Link>
-        <Link href='/tools/crew-tracker'>
-          <div className='group flex flex-col items-center justify-center rounded border border-primary p-5 hover:bg-primary'>
-            <h2 className='group-hover:text-primary-foreground'>
-              Crew Tracker
-            </h2>
-            <p className='text-md group-hover:text-primary-foreground'>
-              Easily keep track of all your crews!
-            </p>
-          </div>
-        </Link>
-        <Link href='/tools/trader-dashboard'>
-          <div className='group flex flex-col items-center justify-center rounded border border-primary p-5 hover:bg-primary'>
-            <h2 className='group-hover:text-primary-foreground'>
-              Trader Dashboard
-            </h2>
-            <p className='text-md group-hover:text-primary-foreground'>
-              Keep track of your trading activities!
-            </p>
-          </div>
-        </Link>
-        <Link href='/tools/public-buildings'>
-          <div className='group flex flex-col items-center justify-center rounded border border-primary p-5 hover:bg-primary'>
-            <h2 className='group-hover:text-primary-foreground'>
-              Public Buildings
-            </h2>
-            <p className='text-md group-hover:text-primary-foreground'>
-              Find buildings for public use!
-            </p>
-          </div>
-        </Link>
-        <Link href='/tools/ships-for-sale'>
-          <div className='group flex flex-col items-center justify-center rounded border border-primary p-5 hover:bg-primary'>
-            <h2 className='group-hover:text-primary-foreground'>
-              Ships For Sale
-            </h2>
-            <p className='text-md group-hover:text-primary-foreground'>
-              Find all ships that are currently for sale!
-            </p>
-          </div>
-        </Link>
+      <div className='grid grid-cols-1 gap-3 md:grid-cols-2 xl:grid-cols-3'>
+        <ToolLink
+          href='/tools/process-finder'
+          title='Process Finder'
+          description='Find out what you can make from your leftover products!'
+          icon={Cpu}
+        />
+        <ToolLink
+          href='/tools/production-tracker'
+          title='Production Tracker'
+          description='Easily keep track of all your running production facilities!'
+          icon={Cog}
+        />
+        <ToolLink
+          href='/tools/crew-tracker'
+          title='Crew Tracker'
+          description='Easily keep track of all your crews!'
+          icon={UsersRound}
+        />
+        <ToolLink
+          href='/tools/trader-dashboard'
+          title='Trader Dashboard'
+          description='Keep track of your trading activities!'
+          icon={ChartNoAxesCombined}
+        />
+        <ToolLink
+          href='/tools/public-buildings'
+          title='Public Buildings'
+          description='Find buildings for public use!'
+          icon={Building}
+        />
+        <ToolLink
+          href='/tools/ships-for-sale'
+          title='Ships For Sale'
+          description='Find all ships that are currently for sale!'
+          icon={Rocket}
+        />
       </div>
     </div>
   )
 }
+type ToolLinkProps = {
+  href: Route
+  title: string
+  description: string
+  icon: ComponentType<LucideProps>
+}
+const ToolLink = ({ href, title, description, icon: Icon }: ToolLinkProps) => (
+  <Link href={href}>
+    <div className='group flex h-full w-full items-center gap-x-5 rounded border border-primary p-5 hover:bg-primary'>
+      <Icon size={48} className='group-hover:text-primary-foreground' />
+      <div className='flex w-full flex-col items-center justify-center'>
+        <h2 className='group-hover:text-primary-foreground'>{title}</h2>
+        <p className='text-md group-hover:text-primary-foreground'>
+          {description}
+        </p>
+      </div>
+    </div>
+  </Link>
+)
