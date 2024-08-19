9e183fac35aadcc93dcbf79140d9a34b3a5cd7b1
Jannis Isensee <jannis.isensee@gmail.com>
2024-08-19 20:29:28 +0200

    improve abundances chart

--- src/app/asteroids/[id]/abundances-chart.tsx
+++ src/app/asteroids/[id]/abundances-chart.tsx
@@ -1,5 +1,6 @@
 import { D } from '@mobily/ts-belt'
 import { Bar, BarChart, XAxis, YAxis } from 'recharts'
+import { useEffect, useState } from 'react'
 import { LotAbundances } from './hooks'
 import { ResourceAbundanceList } from './resource-abundance-list'
 import {
@@ -7,6 +8,12 @@ import {
   ChartTooltip,
   ChartTooltipContent,
 } from '@/components/ui/chart'
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip'
 
 export type AbundancesChartProps = {
   asteroidAbundances: LotAbundances[]
@@ -17,19 +24,46 @@ export const AbundancesChart = ({
   asteroidAbundances,
   selectedResource,
 }: AbundancesChartProps) => {
+  const [copiedLot, setCopiedLot] = useState<number>()
+
+  const onLotCopy = (lot: number) => {
+    navigator.clipboard.writeText(lot.toString())
+    setCopiedLot(lot)
+  }
+
+  useEffect(() => {
+    if (copiedLot) {
+      const timeout = setTimeout(() => setCopiedLot(undefined), 3_000)
+      return () => clearTimeout(timeout)
+    }
+  }, [copiedLot])
+
   return (
     <div className='space-y-3'>
+      <TooltipProvider>
+        <Tooltip open={!!copiedLot}>
+          <TooltipTrigger>
+            <p className='italic'>
+              Hover over a bar to see the abundances for that lot. Click it to
+              copy the lot number to your clipboard.
+            </p>
+          </TooltipTrigger>
+          <TooltipContent>Lot number copied to your clipboard.</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
       <ChartContainer className='max-h-[50vh] w-full' config={{}}>
-        <BarChart data={asteroidAbundances} margin={{ left: 12, right: 12 }}>
+        <BarChart
+          data={asteroidAbundances}
+          margin={{ left: 12, right: 12 }}
+          onClick={(d) => d.activeLabel && onLotCopy(parseInt(d.activeLabel))}
+        >
           <XAxis dataKey='lotIndex' />
           {selectedResource > 0 && (
             <YAxis tickFormatter={(value) => (value * 100).toFixed(1) + '%'} />
           )}
           <ChartTooltip
-            cursor={false}
             content={
               <ChartTooltipContent
-                indicator='dot'
                 formatter={(_value, _name, item) => {
                   const abundances = item.payload as LotAbundances
                   return (
